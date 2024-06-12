81791f94cc322a18c347b3ceaa106ab1a0b31696
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-06-12 16:24:10 -0400

    biome

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,8 @@ import {
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
-import { routes } from "./app.routes";
 import { provideClientHydration } from "@angular/platform-browser";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
 	providers: [
